6d70f654a44700cde25171f2b64bbb3cecca91f0
manggalaputraaji <manggalaputraaji@gmail.com>
2026-04-15 20:43:53 +0700

    REVminor 4.3

--- script.js
+++ script.js
@@ -1,22 +1,16 @@
 const scriptURL = 'https://script.google.com/macros/s/AKfycbyzAzzp27sudKWsncEd9n_kUBcK7ZPuic7UXLxzsHppORE0y7JeAuAuJSXItVvZctX2/exec';
 const form = document.getElementById('formPelanggaran');
 const btn = document.getElementById('btnSimpan');
-
 form.addEventListener('submit', e => {
     e.preventDefault();
     btn.disabled = true;
     btn.innerHTML = "Sabar, lagi ngirim...";
-
-    // Mengambil data form dan mengubahnya jadi Object JSON
     const formData = new FormData(form);
     const data = Object.fromEntries(formData.entries());
-    
-    // Tambahkan label agar Apps Script tahu ini input pelanggaran
     data.type = "INPUT_PELANGGARAN";
-
     fetch(scriptURL, { 
         method: 'POST', 
-        body: JSON.stringify(data) // Kirim sebagai JSON
+        body: JSON.stringify(data)
     })
     .then(response => {
         alert('Data berhasil masuk ke Google Sheets!');
